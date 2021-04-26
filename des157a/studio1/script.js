f84a86ad116e41113d6f476b88383a1a26ec7d2e
patelkajal18 <kajpatel@ucdavis.edu>
2021-04-26 16:01:37 -0700

    done w madlibs

--- des157a/studio1/script.js
+++ des157a/studio1/script.js
@@ -22,22 +22,18 @@
             let time  = document.querySelector('#right1 h5');
 
             logo.style.color = 'white';
-            changeBackgroundContext(background, snack);
-         
-            justice.innerHTML = `Justice <b>${tvChar}</b> Elected To Supreme Court`;
+            changeBackgroundContext(background, snack, logo);
+            
+            justice.innerHTML = `Justice <span style="font-weight:800;">${tvChar}</span> Elected To Supreme Court`;
             if (background == "clouds") {
                 justice.style.color = "black";
                 justiceAuthor.style.color = "black";
-                document.querySelector('h2').style.color = "white";
             } else {
                 justice.style.color = "white";
                 justiceAuthor.style.color = "white";
-                document.querySelector('h2').style.color = "black";
             }
-            partner.innerHTML = `IN PARTNERSHIP WITH THE WORLD <b>${houseObj.toUpperCase()}</b> COUNCIL`;
+            partner.innerHTML = `IN PARTNERSHIP WITH THE WORLD <span style="font-weight:800;">${houseObj.toUpperCase()}</span> COUNCIL`;
             time.innerHTML = `April 20, <b>30${numYr}</b>`;
-
-
         }
     });
 
@@ -54,7 +50,7 @@
         return true;
     }
 
-    async function changeBackgroundContext(image, food) {
+    async function changeBackgroundContext(image, food, logo) {
         let mag = document.getElementById('mag');
   
         let midText = document.querySelector('#center1 h3');
@@ -62,21 +58,24 @@
         switch(image) {
             case 'stones': 
                 mag.style.backgroundImage = "url('./images/rocks.jpg')"; 
-                midText.innerHTML = `Overpopulation of <b>${food}</b> in California Becomes Detrimental`;
+                midText.innerHTML = `Overpopulation of <span style="font-weight:800;">${food}</span> in California Becomes Detrimental`;
                 midText.style.color = "white";
                 midAuthor.style.color = "white";
+                logo.style.color = "#d50000";
                 break;
             case 'clouds': 
                 mag.style.backgroundImage = "url('./images/sky.jpg')"; 
-                midText.innerHTML = `Overpopulation of <b>${food}</b> in New York Becomes Detrimental`;
+                midText.innerHTML = `Overpopulation of <span style="font-weight:800;">${food}</span> in New York Becomes Detrimental`;
                 midText.style.color = "black";
                 midAuthor.style.color = "black";
+                logo.style.color = "white";
                 break;
-            default:
+            default: //ocean
                 mag.style.backgroundImage = "url('./images/ocean.jpg')"; 
-                midText.innerHTML = `Overpopulation of <b>${food}</b> in Hawaii Becomes Detrimental`;
+                midText.innerHTML = `Overpopulation of <span style="font-weight:800;">${food}</span> in Hawaii Becomes Detrimental`;
                 midText.style.color = "white";
                 midAuthor.style.color = "white";
+                logo.style.color = "black";
                 break;
           }
     } 
@@ -91,6 +90,5 @@
         }
         return words.join(" ");
     }
-    
 })();
     

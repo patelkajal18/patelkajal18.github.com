458e0f95423adc798ceac74996b94827b68fb88e
patelkajal18 <kajpatel@ucdavis.edu>
2021-05-21 22:37:26 -0700

    done w code

--- des157a/studio3/script.js
+++ des157a/studio3/script.js
@@ -3,12 +3,16 @@
     let startGame = document.getElementById('startgame');
     let gameControl = document.getElementById('gamecontrol');
     let game = document.getElementById('game');
-    let score = document.getElementById('score');
+    let score = document.getElementById('gamescore');
+    let score1 = document.getElementById('score1');
+    let score2 = document.getElementById('score2');
     let actionArea = document.getElementById('actions');
+    let player1Section = document.getElementById('player1');
+    let player2Section = document.getElementById('player2');
 
     let gameData = {
-        dice: ['./images/1die.png', './images/2die.png', '3die.jpg', 
-            '4die.jpg', '5die.jpg', '6die.jpg'], //change this to elements photos.
+        dice: ['./images/fire1.png', './images/air2.png', './images/earth3.png', 
+            './images/water4.png'], //change this to elements photos.
         players: ['player 1', 'player 2'],
         score: [0, 0],
         roll1: 0,
@@ -20,8 +24,10 @@
     };
 
     startGame.addEventListener("click", function(){
-        gameControl.innerHTML = '<h2>The Game Has Started</h2>';
-        gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
+        gameControl.innerHTML = '<h2 style="font-size: 30px;">... game has started ...</h2>';
+        gameControl.innerHTML += '<button id="quit">quit</button>';
+        player1Section.style.visibility = "visible";
+        player2Section.style.visibility = "visible";
 
         document.getElementById('quit').addEventListener('click', function(){
             location.reload();
@@ -36,7 +42,9 @@
 
     function setUpTurn() {
         game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
-        actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
+        game.innerHTML = `<img class="card" src="./images/blank.png"> <img class="card" src="./images/blank.png">`;
+        actionArea.innerHTML = `<button style="width: 400px; font-size: 25px" id="roll">shuffle cards for ${gameData.players[gameData.index]}</button>`;
+        
         document.getElementById('roll').addEventListener('click', function(){
             console.log("roll the dice");
             throwDice();
@@ -45,32 +53,33 @@
 
     function throwDice() {
         actionArea.innerHTML = '';
-        gameData.roll1 = Math.floor(Math.random() * 6) + 1;
-        gameData.roll2 = Math.floor(Math.random() * 6) + 1;
+        gameData.roll1 = Math.floor(Math.random() * 4) + 1;
+        gameData.roll2 = Math.floor(Math.random() * 4) + 1;
         game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
-        game.innerHTML = `<img src="${gameData.dice[gameData.roll1-1]}"> <img src="${gameData.dice[gameData.roll2-1]}">`;
+        game.innerHTML = `<img class="card" src="${gameData.dice[gameData.roll1-1]}"> <img class="card" src="${gameData.dice[gameData.roll2-1]}">`;
         gameData.rollSum = gameData.roll1 + gameData.roll2;
         showCurrentScore(); //?
         
         if(gameData.rollSum == 2) {
             console.log("snake eyes were rolled");
-            game.innerHTML += '<p>Oh snap! Snake eyes!</p>';
-            
+            game.innerHTML += '<p style="font-size: 25px; background-color: #ffffff67; margin-top: 20px; width: 250px; transform: translate(60%, 0%); text-align: center;">oh snap! snake eyes!</p>';
             gameData.score[gameData.index] = 0;
             growTree("death", gameData.index, 0);
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
             setTimeout(setUpTurn, 2000);
+
         } else if (gameData.roll1 == 1 || gameData.roll2 == 1) {
             console.log('one of the two dice was a 1');
             growTree("stop", gameData.index, gameData.score[gameData.index]);
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            game.innerHTML += `<p>Sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</p>`;
+            game.innerHTML += `<p id="message" style="text-align: center; font-size: 20px; background-color: #ffffff67;; margin-top: 20px;">sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</p>`;
             setTimeout(setUpTurn, 2000);
+
         } else {
             console.log("the game proceeds");
             gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
             growTree("grow", gameData.index, gameData.score[gameData.index]);
-            actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
+            actionArea.innerHTML = '<button id="rollagain">play</button> <button id="pass">pass</button>';
             document.getElementById('rollagain').addEventListener('click', function(){
                 throwDice();
             });
@@ -88,35 +97,40 @@
         if(gameData.score[gameData.index] > gameData.gameEnd) {
             score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
             actionArea.innerHTML = '';
-            document.getElementById('quit').innerHTML = "Start a New Game?";
+            gameControl.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points</h2>` + '<button id="quit">quit</button>'
+            showCurrentScore();
+            document.getElementById('quit').innerHTML = "<button style='width: 130px'> new game </button>";
+            document.getElementById('quit').addEventListener('click', function(){
+                location.reload();
+                console.log("set up the turn!");
+                setUpTurn();
+            });
+            
         } else {
             showCurrentScore();
         }
     }
 
     function showCurrentScore() {
-        score.innerHTML =  `<p>The score is currently <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.players[1]} ${gameData.score[1]}</strong></p>`;
+        score1.innerHTML = `<p><strong>${gameData.score[0]}</strong><\p>`;
+        score2.innerHTML = `<p><strong>${gameData.score[1]}</strong><\p>`;
     }
 
     function growTree(condition, whichPlayer, score) {
         let tree = document.getElementById(`${gameData.tree[whichPlayer]}`);
         switch(condition) {
             case "death":
-                tree.src = "./images/";
+                tree.src = "./images/treeDead.png";
                 break;
             case "grow":
-                if (score > 29) {         // fully grown tree
-                    tree.src = "./images/";
-                } else if (score > 23) {  // large tree
-                    tree.src = "./images/";
-                } else if (score > 17) {  // medium tree
-                    tree.src = "./images/";
-                } else if (score > 11) {  // small tree
-                    tree.src = "./images/treeSmall.png";
-                } else if (score > 5) {   // small plant
-                    tree.src = "./images/plantSmall.png";
-                } else {                  // seedling
-                    tree.src = "./images/";
+                if (score > 25) {  // large tree
+                    tree.src = "./images/treeL.png";
+                } else if (score > 15) {  // medium tree
+                    tree.src = "./images/treeM.png";
+                } else if (score > 5) {                  // small tree
+                    tree.src = "./images/treeS.png";
+                } else {
+                    tree.src = "./images/treeDead.png";
                 }
                 break;
             default: break;
